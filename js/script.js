bb86bfb4e3adba20b7714fb5bfe83545436c515b
Daniel <danbsilva98@gmail.com>
2020-11-26 12:46:42 -0300

    processo de criação do modal

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import ScrollAnima from './modules/scroll-anima.js';
+import Modal from './modules/modal.js';
 
 const scrollSuave = new ScrollSuave('.menu a[href^="#"]');
 scrollSuave.init();
 
 const scrollAnima = new ScrollAnima('.js-scroll');
 scrollAnima.init();
+
+const modal = new Modal('section.projetos .projeto-box', '.modal .fechar', '.modal-container');
+modal.init();
+
+
+
